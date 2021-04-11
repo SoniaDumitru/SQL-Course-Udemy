2809072c58b1f5ceca2e33582b364f016c96737d
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-04-11 18:35:17 -0500

    select with date

--- group_by_statements.sql
+++ group_by_statements.sql
@@ -22,10 +22,29 @@ SELECT SUM(replacement_cost) FROM film;
 -- 2. categorical column is non-continuous 
 
 -- SELECT category_column, AGG(data_col) FROM table GROUP BY category col;
-
 SELECT company, division, SUM(sales)
 FROM finance_table 
 WHERE division IN ('marketing', 'transport')
 GROUP BY company, division;
+
+SELECT customer_id FROM payment
+GROUP BY customer_id;
+
+SELECT customer_id FROM payment
+GROUP BY customer_id
+ORDER BY customer_id;
+
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) ASC;
+
+SELECT customer_id, staff_id, SUM(amount) FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY staff_id, customer_id;
+
+-- SELECT just the date information with DATE
+SELECT DATE(payment_date), SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC;
 
 
