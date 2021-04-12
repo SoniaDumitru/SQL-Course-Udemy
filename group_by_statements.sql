ecd1420c098908ffb75c4725edebb2c68aa44001
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-04-11 22:51:00 -0500

    group by, sum order by

--- group_by_statements.sql
+++ group_by_statements.sql
@@ -50,4 +50,13 @@ ORDER BY SUM(amount) DESC;
 SELECT staff_id, COUNT(*) FROM payment
 GROUP BY staff_id;
 
+SELECT rating, ROUND(AVG(replacement_cost), 2) FROM film
+GROUP BY rating;
+
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(AMOUNT)
+LIMIT 10;
+
 
