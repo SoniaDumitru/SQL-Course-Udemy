b013cbd92e9baa8c197911f0c3bab38c60af9c7c
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-04-21 21:57:18 -0500

    timestamps, extract date info

---
+++ functions_and_tools.sql
@@ -0,0 +1,18 @@
+-- TIMESTAMPS AND EXTRACT (useful when creating rather and quering)
+-- TIME - only time
+-- DATE - only date 
+-- TIMESTAMP - combines both date and time
+-- TIMESTAMPZ - date and time plus time zone
+
+-- TIMEZONE
+-- NOW
+-- TIMEOFDAY
+-- CURRENT_TIME
+-- CURRENT_DATE
+
+-- SHOW ALL (to see all params) ex: SHOW TIMEZONE;
+
+SELECT NOW();
+SELECT TIMEOFDAY();
+SELECT CURRENT_TIME;
+SELECT CURRENT_DATE;
