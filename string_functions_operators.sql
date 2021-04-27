e22ed5b068e36906cc207fcb13da32d027732190
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-04-26 21:57:03 -0500

    more examples with subqueries

--- string_functions_operators.sql
+++ string_functions_operators.sql
@@ -5,3 +5,6 @@ SELECT LENGTH(first_name) FROM customer LIMIT 10;
 SELECT first_name || last_name FROM customer;
 SELECT first_name || ' ' || last_name FROM customer;
 
+-- How to create a custom email address:
+SELECT LOWER(LEFT(first_name,1)) || LOWER(last_name) || '@gmail.com' AS custom_email FROM customer;
+
