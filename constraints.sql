a5e22bb1508c97452d0f3985aced6af0dd68210c
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-05-03 23:19:42 -0500

    practice insert into tables

--- constraints.sql
+++ constraints.sql
@@ -50,4 +50,17 @@ CREATE TABLE account_job(
 	hire_date TIMESTAMP
 );
 
+-- INSERT 
+INSERT INTO account(username, password, email, created_on)
+VALUES ('Jose', 'password', 'jose@mail.com', CURRENT_TIMESTAMP);
+
+INSERT INTO job(job_name)
+VALUES ('Austronaut');
+
+INSERT INTO job(job_name)
+VALUES ('President');
+
+INSERT INTO account_job(user_id, job_id, hire_date)
+VALUES(1,1,CURRENT_TIMESTAMP);
+
 
