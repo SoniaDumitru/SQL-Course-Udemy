a5897c668d12d4b705fc7efb84b61b07dffc1e49
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-05-03 22:55:17 -0500

    contrains and create table

---
+++ constraints.sql
@@ -0,0 +1,53 @@
+-- Constrains are rules inforced on data columns on a table
+-- used to prevent invalid data from enetering into database
+-- accuracy and reliability of the data 
+
+-- there are two types: column constrains & table constrains
+
+-- COLUMN:
+-- NOT NULL Constraint
+-- UNIQUE Constraint 
+-- CHECK Constraint (all values in the column satisfy certain condition)
+-- EXCLUSION (if any two rows dont return true when using a specified operator)
+
+-- TABLE: 
+-- CHECK Condition
+-- REFERENCES another column in another table
+-- UNIQUE (unique among multiple column)
+-- PRIMARY KEY(column_list)
+
+
+-- CREATE TABLE
+CREATE TABLE table_name(
+    column_name TYPE column_constraint,
+    column_name TYPE column_constraint,
+    table_contrains table_constrains
+)INHERITS existing table_table_name;
+
+CREATE TABLE players(
+    player_id SERIAL PRIMARY KEY,
+    age SMALLINT NOT NULL
+);
+
+-- CREATE tables and connections (primary and foreign keys attached)
+CREATE TABLE account(
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	email VARCHAR(250) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	last_login TIMESTAMP
+);
+
+CREATE TABLE job(
+	job_id SERIAL PRIMARY KEY,
+	job_name VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE account_job(
+	user_id INTEGER REFERENCES account(user_id),
+	job_id INTEGER REFERENCES job(job_id),
+	hire_date TIMESTAMP
+);
+
+
