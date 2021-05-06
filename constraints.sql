876a9bc96d11b88dadca1c5596e37c650f9d1bec
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-05-05 22:53:51 -0500

    creting a table with constraints

--- constraints.sql
+++ constraints.sql
@@ -50,6 +50,12 @@ CREATE TABLE account_job(
 	hire_date TIMESTAMP
 );
 
+CREATE TABLE information(
+	info_id SERIAL PRIMARY KEY,
+	title VARCHAR(500) NOT NULL,
+	person VARCHAR(50) NOT NULL UNIQUE
+);
+
 -- INSERT 
 INSERT INTO account(username, password, email, created_on)
 VALUES ('Jose', 'password', 'jose@mail.com', CURRENT_TIMESTAMP);
@@ -79,3 +85,47 @@ FROM account
 WHERE account_job.user_id = account.user_id;
 
 -- DELETE ROWS
+DELETE FROM job 
+WHERE job_name = 'Cowboy'
+RETURNING job_id, job_name;
+
+-- ALTER 
+-- RENAME TABLE
+ALTER TABLE information 
+RENAME TO new_info;
+
+-- RENAME COLUMN
+ALTER TABLE new_info
+RENAME COLUMN person TO people;
+
+-- REMOVE A CONSTRAINT 
+ALTER TABLE new_info
+ALTER COLUMN people DROP NOT NULL;
+
+-- DROP COLUMN 
+ALTER table new_info
+DROP COLUMN people;
+
+ALTER table new_info
+DROP COLUMN IF EXISTS people;
+
+-- Constraints when inserting into a table
+CREATE TABLE employees(
+    emp_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL, 
+    birthdate DATE CHECK (birthdate > '1900-01-01'),
+    hire_date DATE CHECK (hire_date > birthdate),
+    salary INTEGER CHECK (salary > 0)
+);
+
+INSERT INTO employees(
+	first_name,
+	last_name,
+	birthdate,
+	hire_date,
+	salary
+)
+VALUES (
+	'Sonia', 'Dumitru', '1899-11-03', '2010-01-01', 100
+); -- ERROR:  new row for relation "employees" violates check constraint "employees_birthdate_check"
